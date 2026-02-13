24018b73b11427520a5e8cba95f80579f15ba7c0
sheikharifulislam <sheikharifulislam191@gmail.com>
2026-02-14 03:04:42 +0600

    style(stylelint): allow Tailwind CSS at-rules

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import("stylelint").Config} */
 export default {
   extends: ['stylelint-config-standard'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'source',
+          'theme',
+          'utility',
+          'variant',
+          'custom-variant',
+          'plugin',
+          'layer',
+          'apply',
+        ],
+      },
+    ],
+  },
 };
